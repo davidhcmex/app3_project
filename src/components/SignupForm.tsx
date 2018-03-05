dba80e753b4a867454fa236e5a1f82ff5dd0a1cb
david <davidhcgeo@gmail.com>
2018-03-05 14:21:49 -0800

    with signup from almost ready

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,21 +1,93 @@
 import * as React from "react";
 
-export class SignupForm extends React.Component {
-    render() {
-        return(
-        <form>
-            <h1>Join Our Community</h1>
-            <div className="form-group">
-                <label className="control-label">Username</label>
-                <input type="text" name="username" className="form-control" />
+interface user_info {
+    username: string,
+    email: string,
+    password: string,
+    passwordConfirmation: string
+}
 
-            </div>
-            <div className="form-group">
-                <button className="btn btn-primary btn-lg">
-                    Signup
+export class SignupForm extends React.Component<{}, user_info> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            username: "",
+            email: "",
+            password: "",
+            passwordConfirmation: ""
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onSubmit(e: React.FormEvent<EventTarget>) {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+    onChange(e: React.FormEvent<EventTarget>) {
+    
+        this.setState(
+            {
+                username: this["uname"].value,
+                email: this["email"].value,
+                password: this["passwd"].value,
+                passwordConfirmation: this["passwdCnf"].value,
+
+            })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <h1>Join Our Community</h1>
+                <div className="form-group">
+                    <label className="control-label">Username</label>
+                    <input value={this.state.username}
+                        ref={node => this["uname"] = node}
+                        type="text"
+                        name="username"
+                        onChange={this.onChange}
+                        className="form-control" />
+
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Email</label>
+                    <input value={this.state.email}
+                        type="text" name="email"
+                        ref={node => this["email"] = node}
+                        onChange={this.onChange}
+                        className="form-control" />
+
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Password</label>
+                    <input value={this.state.password}
+                        type="password" name="password"
+                        ref={node => this["passwd"] = node}
+                        onChange={this.onChange}
+                        className="form-control" />
+
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Confirm Password</label>
+                    <input value={this.state.passwordConfirmation}
+                        type="password" name="passwordConfirmation"
+                        ref={node => this["passwdCnf"] = node}
+                        onChange={this.onChange}
+                        className="form-control" />
+
+                </div>
+
+                <div className="form-group">
+                    <button className="btn btn-primary btn-md">
+                        Signup
                 </button>
-            </div>
-        </form>
+                </div>
+            </form>
         )
     }
 }
